4feba8826dc8fe3847380741b538e1dfc1cb3a2d
Suraj <122242670+Kiran9991@users.noreply.github.com>
2023-11-07 19:37:02 +0530

    Adder Header

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>ReactMeals</h1>
+      <div className="cart">Your Cart</div>
+    </header>
+  );
+};
+
+export default Header;
